d490946ca7faaac74d6c47e100c768e5244f7a49
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2013-10-29 22:35:52 -0600

    Esqueleto del servidor TCP, solo manda un eco

--- VerticleManager.groovy
+++ VerticleManager.groovy
@@ -1,2 +1,9 @@
 def log = container.logger
 log.info "Inicia el verticle manager"
+
+def port = 4433
+vertx.createNetServer().connectHandler { sock ->
+    sock.dataHandler { buffer ->
+        sock.write(buffer)
+    }
+}.listen(port)
