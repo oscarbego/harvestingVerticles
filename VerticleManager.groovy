6300f54740e2ddb9309b5724d758971635d16c3f
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2013-10-29 22:40:18 -0600

    Se usa el eventBus para mandar el eco por el socket.

--- VerticleManager.groovy
+++ VerticleManager.groovy
@@ -1,9 +1,23 @@
 def log = container.logger
+def port = 4433
+def sendEchoAddress = 'send-echo'
+def eb = vertx.eventBus
+
 log.info "Inicia el verticle manager"
 
-def port = 4433
+def sendEcho = { msg ->
+    eb.send(sendEchoAddress, msg.toString())
+}
+
+def registerEchoSender = { sock ->
+    eb.registerHandler(sendEchoAddress) { msg ->
+        sock.write(msg.body())
+    }
+}
+
 vertx.createNetServer().connectHandler { sock ->
+    registerEchoSender(sock)
     sock.dataHandler { buffer ->
-        sock.write(buffer)
+        sendEcho buffer
     }
 }.listen(port)
