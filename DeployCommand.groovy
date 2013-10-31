c97a67dea78c4751ae0df7f3c1c361710eabc0a1
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2013-10-30 20:51:28 -0600

    Ya despliega cualquier verticle que este en el directorio.

--- DeployCommand.groovy
+++ DeployCommand.groovy
@@ -1,3 +1,5 @@
+import groovy.io.FileType
+
 def log = container.logger
 def eb = vertx.eventBus
 
@@ -7,12 +9,34 @@ def sendEcho = { msg ->
     eb.send('send-echo', msg.toString())
 }
 
+def getVerticlesFilesNames = {
+	def verticlesFilesNames = []
+	new File(".").eachFileMatch(FileType.FILES, ~/^.*groovy$/) { file ->
+	    verticlesFilesNames << file.name
+	}
+	return verticlesFilesNames
+}
+
 def handler = { msg ->
 	def command = msg.body().command
 	def arguments = msg.body().arguments
 	
 	if(command == 'deploy') {
-		sendEcho "deploy ${arguments}"
+		def verticlesFound = []
+		arguments.each { ar ->
+			verticlesFound << getVerticlesFilesNames().findAll { vf ->
+				vf.toLowerCase().startsWith(ar)
+			}
+		}
+		verticlesFound.flatten().each { vn ->
+			container.deployVerticle(vn) { asyncResult ->
+                if(asyncResult.succeeded()) {
+                    sendEcho "[DEPLOY:OK] ${vn} :: ${asyncResult.result()}"
+                } else {
+                    sendEcho "[DEPLOY:ERROR] ${vn} :: ${asyncResult.cause()}"
+                }
+            }
+		}
 	}
 }
 
